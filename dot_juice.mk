7e5b44ed138d232c10311958a4a78050a24e5f4e
c3eru <yyprjkt@gmail.com>
2022-04-09 05:58:01 +0700

    juice: build Hotword only for gapps variant

--- dot_juice.mk
+++ dot_juice.mk
@@ -13,8 +13,22 @@ TARGET_INCLUDE_PIXEL_CHARGER  := true
 TARGET_SUPPORTS_BLUR := true
 
 # Gapps
-#TARGET_GAPPS_ARCH := arm64
 #WITH_GAPPS := true
+ifeq ($(WITH_GAPPS), true)
+TARGET_GAPPS_ARCH := arm64
+# Hotword
+PRODUCT_PACKAGES += \
+    HotwordEnrollmentOKGoogleHEXAGON \
+    HotwordEnrollmentXGoogleHEXAGON
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/hotword/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/hotword/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
+    $(LOCAL_PATH)/hotword/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
+    $(LOCAL_PATH)/hotword/com.android.hotwordenrollment.common.util.jar:$(TARGET_COPY_OUT_SYSTEM_EXT)/framework/com.android.hotwordenrollment.common.util.jar
+else
+#TARGET_GAPPS_ARCH := arm64
+endif
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := dot_juice
